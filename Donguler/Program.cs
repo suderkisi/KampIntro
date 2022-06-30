f91e4c26834fd56b878b61684bc6f421fd5a8492
Sude <suderkisi@gmail.com>
2022-07-01 00:16:04 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Donguler
+
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kurslar = new string[] { "Yazılım Geliştirici Kampı", "Java", "Programlama Temel Kurs", "Python" };
+
+            for (int i = 0; i <kurslar.Length;i++)
+            {
+                Console.WriteLine(kurslar[i]);
+
+            }
+            Console.WriteLine("For bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("sayfa sonu-footer");
+        }
+
+    }
+}
+
---
+++ KampIntro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace kampIntro 
+{ 
+    
+    class Program
+    {
+        static void Main(string[] args) 
+        { 
+            Console.WriteLine("Hello, World!");
+        } 
+
+    }
+}
+
